e0b1e36be2ef67b16c2f1944a9314130e74031c3
Deepak Shahi <96306408+dee8837@users.noreply.github.com>
2022-11-04 10:20:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import { faAirbnb } from '@fortawesome/free-brands-svg-icons'
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>About Us</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum indus. </p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unIt was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an un</p>
+      
+
+
+         {/* <h1>The Evolution of Airbnb's Frontend</h1>
+         <p>@spikebrnn</p>
+         <img src="" alt=""/>
+         <i class="fa-brands fa-airbnb"></i>
+         <FontAwesomeIcon  fontSize={30} icon={faAirbnb} />
+         <p>airbnb</p>
+ */}
+
+        </div>
+  );
+}
+
+export default App;
